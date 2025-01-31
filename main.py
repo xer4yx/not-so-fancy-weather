dbe1a411f451b0d2bfe2884dd134c47f410cf0de
xerayx <202111173@fit.edu.ph>
2025-01-31 21:43:14 +0800

    feat(cors): added frontend address to allowed origin in CORS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from version import __version__
 
@@ -10,6 +11,19 @@ app = FastAPI(
     title=basic_settings.APP_NAME,
     description="NotSoFancyWeather(NSFW) API",
     version=__version__
+)
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(weather_router, prefix="/api")
